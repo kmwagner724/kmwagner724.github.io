915db044a4b44cdde3a226711987ab9441f31d53
kmwagner724 <kwagner2016@kellogg.northwestern.edu>
2016-02-03 11:52:15 -0600

    final project done

--- list.js
+++ list.js
@@ -1,11 +1,64 @@
 // This code runs when the page loads
 $(function() {
 
-  $("#do-something").on("click", function(event) {
-    // Do something -- jquery selector is binding itself to something with the id "do-something"
-    //.on means the function occurs when we do something (on click)
-    event.preventDefault(); //prevent going to Google.com
-    $("#unicorn").fadeIn(10000); //now our unicorn will show upp when we click the button
-  });
+  $("#remove").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
+})
+// This code runs when the page loads
+$(function() {
 
+  $("#remove2").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
+})
+// This code runs when the page loads
+$(function() {
+
+  $("#remove3").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
+})
+// This code runs when the page loads
+$(function() {
+
+  $("#remove4").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
+})
+$(function() {
+
+  $("#remove5").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
+})
+$(function() {
+
+  $("#remove6").on("click", function(event) {
+    event.preventDefault(); //always do this so nothing bad happens!!!
+    var elementThatWasClicked = $(this); //"this" represents the element that was clicked - gives us a jquery reference to clicked element
+    console.log(elementThatWasClicked); //writing out element that was clicked to the console so we can ee what s happening
+    elementThatWasClicked.parent().remove(); //removing clicked element
+  })
+//now we can click on pictures and as they are clicked they disappear
 })
